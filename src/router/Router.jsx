9d1284996a318ebd24ddba679bec20c410c0e9db
g0th <155659333+g0T-h@users.noreply.github.com>
2024-03-10 13:14:25 +0300

    Update Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,7 +6,6 @@ import Err from "../components/Err"
 import Watch from "../components/Watch"
 import Results from "../components/Results"
 import Footer from "../components/Footer"
-import Movies from "../components/Movies"
 import TopAiring from "../components/TopAiring"
 import Genres from "../components/Genres"
 import Genre from "../components/Genre"
@@ -19,7 +18,6 @@ const Router = () => {
                 <Route path="/" element={<App/>}/>
                 <Route path="/top" element={<TopAiring/>}/>
                 <Route path="/genres" element={<Genres/>}/>
-                <Route path="/movies" element={<Movies/>}/>
                 <Route path="/results/:search" element={<Results/>}/>
                 <Route path="/info/:id" element={<AnimeInfo/>}/>
                 <Route path="/watch/:epID" element={<Watch/>}/>

3b3eb139cc88b94da9c3790a8eda263f200a4fbd
g0th <155659333+g0T-h@users.noreply.github.com>
2024-03-10 14:37:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Analytics } from "@vercel/analytics/react"
 
 // https://vitejs.dev/config/
 export default defineConfig({

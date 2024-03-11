ab12681e5c64277cf8a7786d3ddb3825c95aa0ea
g0th <155659333+g0T-h@users.noreply.github.com>
2024-03-11 21:36:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import { Container } from 'react-bootstrap'
-import MainPage from './components/MainPage'
+// App.jsx
+import './App.css';
+import { Container } from 'react-bootstrap';
+import MainPage from './components/MainPage';
+import UrlPreview from './UrlPreview'; // Import the UrlPreview component
 
-function App()
-{
-
+function App() {
   return (
     <Container>
-      <MainPage/>
+      <MainPage />
+      {/* Example usage of UrlPreview component */}
+      <UrlPreview url="https://example.com" />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;

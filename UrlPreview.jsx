85029fb0b744c8b39ecb15d87119920d77b4f93b
g0th <155659333+g0T-h@users.noreply.github.com>
2024-03-11 21:51:07 +0300

    Update UrlPreview.jsx

--- UrlPreview.jsx
+++ UrlPreview.jsx
@@ -1,60 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const UrlPreview = ({ url }) => {
-  const [previewData, setPreviewData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchUrlPreview = async () => {
-      try {
-        const response = await fetch(`https://api.linkpreview.net/?key=6821509bc3e6e48d225cab98e2c73541&q=${url}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        setPreviewData(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-
-    fetchUrlPreview();
-
-    // Cleanup function
-    return () => {
-      // Cleanup code if needed
-    };
-  }, [url]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (!previewData) {
-    return null;
-  }
-
+const UrlPreview = ({ imageUrl, title, description, url }) => {
   return (
     <div className="url-preview">
-      {previewData.image ? (
-        <img src={previewData.image} alt="Preview" />
-      ) : (
-        <img
-          src={`https://www.google.com/s2/favicons?domain=${new URL(previewData.url).hostname}`}
-          alt="Preview"
-        />
-      )}
+      {imageUrl && <img src={imageUrl} alt="Preview" />}
       <div className="details">
-        <h3>{previewData.title}</h3>
-        <p>{previewData.description}</p>
-        <a href={previewData.url}>Read more</a>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <a href={url}>Read more</a>
       </div>
     </div>
   );

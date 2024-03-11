b5f5f2822d82a68e9e72c3bf49c656f00a1b1b26
g0th <155659333+g0T-h@users.noreply.github.com>
2024-03-11 21:57:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,11 @@
 import './App.css';
 import { Container } from 'react-bootstrap';
 import MainPage from './components/MainPage';
-import UrlPreview from '../UrlPreview'; // Import the UrlPreview component
 
 function App() {
   return (
     <Container>
       <MainPage />
-      {/* Example usage of UrlPreview component */}
-      <UrlPreview
-        url="https://anixen.vercel.app/"
-        title="aniXen"
-        description="AniXen is an anime streaming platform."
-        image="https://github.com/g0T-h/anixen/raw/main/public/FNLogo.png" // You can replace this with the actual image URL
-        />
     </Container>
   );
 }

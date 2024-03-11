82fd6ea1fd7ae4dfa8bfb794e5c397dd901cd39c
g0th <155659333+g0T-h@users.noreply.github.com>
2024-03-11 21:53:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ function App() {
     <Container>
       <MainPage />
       {/* Example usage of UrlPreview component */}
-      <UrlPreview url="https://anixen.vercel.app/" />
+      <UrlPreview
+        url="https://anixen.vercel.app/"
+        title="aniXen"
+        description="AniXen is an anime streaming platform."
+        image="https://github.com/g0T-h/anixen/raw/main/public/FNLogo.png" // You can replace this with the actual image URL
+        />
     </Container>
   );
 }

3626f586acd6a636e5241bbe2b7c5f84554fc231
★ ern <155659333+SH1NSETSU@users.noreply.github.com>
2024-12-14 17:11:29 +0300

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,32 @@
 import { useContext, useEffect, useState } from "react";
 import Context from "../context/Context";
 
-export default function useFetch (link)
-{
-    const {baseURL} = useContext(Context)
-    const [list,setList] = useState()
+export default function useFetch(link) {
+    const { baseURL } = useContext(Context);
+    const [list, setList] = useState();
 
     useEffect(() => {
-        fetch(baseURL+link)
-        .then(res=>res.json())
-        .then(data => setList(data))
-    }, [])
+        const fetchData = async () => {
+            try {
+                const response = await fetch(baseURL + link, {
+                    headers: {
+                        Referer: 'https://s3embtaku.pro', // Specify the Referer header
+                    },
+                });
 
-    return list
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setList(data);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+
+        fetchData();
+    }, [baseURL, link]);
+
+    return list;
 }
